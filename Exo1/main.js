b7f5b0fe15f8f24ce17f4a83432818fda2db7e1b
Aydentt <91367447+Aydentt@users.noreply.github.com>
2021-09-25 11:35:25 +0200

    Exo1

---
+++ Exo1/main.js
@@ -0,0 +1,23 @@
+function checkPrimeNumber() {
+	var i = 2;
+	var nbr = document.getElementById("nbrPremier").value;
+  
+  console.log(nbr)
+  
+  if (nbr=="" || nbr==0 || nbr==1 || nbr==-1) {
+    	document.getElementById("check").innerHTML="erreur Valeur"
+  		return
+  }
+  
+  while (i < (nbr/2) || -i > (nbr/2)) {
+      
+    if (nbr%i === 0) {
+      document.getElementById("check").innerHTML="nonPrime"
+    	return
+     
+    }
+    i++
+	}
+  document.getElementById("check").innerHTML="prime"
+
+}
