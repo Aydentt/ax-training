2db72d875979ffea15acccf8cd2e49ca45c53bc3
Aydentt <91367447+Aydentt@users.noreply.github.com>
2021-10-09 12:43:47 +0200

    office-trm-v2

---
+++ Office-tourisme/officeTourisme.js
@@ -0,0 +1,25 @@
+let hospitality
+let foodDrinks
+let activities
+let transport
+let contact
+
+let hospitality=document.getElementById("hospitality").innerHTML;
+
+let foodDrinks=document.getElementById("foodDrinks").innerHTML;
+
+let activities=document.getElementById("activities").innerHTML;
+
+let transport=document.getElementById("transport").innerHTML;
+
+let contact=document.getElementById("contact").innerHTML;
+
+const mouseEvent = document.querySelector (".mouseEvent");
+const bottom = document.querySelector (".bottom");
+
+mouseEvent.addEventListener (".mousemove" (Event) {
+    bottom.innerHTML = e.x
+})
+
+
+
